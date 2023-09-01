0d3fdeb7b3be4a12d8cae95bad4cfc6dd87aea46
TopoArania <matufabbri@gmail.com>
2023-09-01 19:55:19 -0300

    Created input manager class

---
+++ Desarrollo-3-2023/Assets/Code/Scripts/Input/InputManager.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static event Action<float> onMove;
+    public static event Action onAttack;
+    public static event Action onBlock;
+    public static event Action onJump;
+
+    private void OnMove(InputValue input)
+    {
+        var axis = input.Get<float>();
+        
+        onMove?.Invoke(axis);
+        print("Move: " + axis);
+    }
+
+    private void OnAttack()
+    {
+        onAttack?.Invoke();
+        print("Attack");
+    }
+
+    private void OnBlock()
+    {
+        onBlock?.Invoke();
+        print("Block");
+    }
+
+    private void OnJump()
+    {
+        onJump?.Invoke();
+        print("Jump");
+    }
+}

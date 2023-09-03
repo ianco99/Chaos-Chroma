c1a2621d26fb16474134f84a972a5be25ed39632
ianco99 <71154684+ianco99@users.noreply.github.com>
2023-09-02 22:30:18 -0300

    Created BaseState abstract class

---
+++ Desarrollo-3-2023/Assets/Code/Scripts/FSM/BaseState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseState : IState
+{
+    public BaseState()
+    {
+
+    }
+
+    public void OnEnter()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void OnExit()
+    {
+
+    }
+}

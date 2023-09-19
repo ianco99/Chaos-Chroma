1e0cc39fd39f16d87b386675bb35f496cb3c40be
TopoArania <matufabbri@gmail.com>
2023-09-19 17:54:38 -0300

    Added camera follow

---
+++ Desarrollo-3-2023/Assets/Code/Scripts/Camera/CameraController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Code.Scripts.Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private Vector3 offset = Vector3.zero;
+        [SerializeField] private float speed = 10f;
+
+        private void Update()
+        {
+            if (!target) return;
+            
+            transform.position = Vector3.Lerp(transform.position, target.position + offset, speed * Time.deltaTime);
+        }
+    }
+}

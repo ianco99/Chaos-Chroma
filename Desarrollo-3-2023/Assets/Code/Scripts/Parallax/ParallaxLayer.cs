6d71eda1e4dfbfb2ea3487b850855af5e3c1a40c
TopoArania <matufabbri@gmail.com>
2023-09-19 17:30:26 -0300

    Added summaries to parallax layer

--- Desarrollo-3-2023/Assets/Code/Scripts/Parallax/ParallaxLayer.cs
+++ Desarrollo-3-2023/Assets/Code/Scripts/Parallax/ParallaxLayer.cs
@@ -1,65 +1,82 @@
 using UnityEngine;
 
-public class ParallaxLayer : MonoBehaviour
+namespace Code.Scripts.Parallax
 {
-    [SerializeField] private GameObject[] images;
-    [SerializeField] private Transform character;
-
-    private SpriteRenderer[] sprites;
-    private int[] positions;
-    private float size;
-
-    private void Awake()
+    /// <summary>
+    /// Controls the positioning of the parallax layers
+    /// </summary>
+    public class ParallaxLayer : MonoBehaviour
     {
-        positions = new int[images.Length];
-        sprites = new SpriteRenderer[images.Length];
+        [SerializeField] private GameObject[] images;
+        [SerializeField] private Transform character;
 
-        for (var i = 0; i < images.Length; i++)
+        private SpriteRenderer[] sprites;
+        private int[] positions;
+        private float size;
+
+        private void Awake()
         {
-            sprites[i] = images[i].GetComponent<SpriteRenderer>();
-            positions[i] = i;
+            positions = new int[images.Length];
+            sprites = new SpriteRenderer[images.Length];
+
+            for (var i = 0; i < images.Length; i++)
+            {
+                sprites[i] = images[i].GetComponent<SpriteRenderer>();
+                positions[i] = i;
+            }
+
+            size = sprites[0].bounds.size.x;
         }
 
-        size = sprites[0].bounds.size.x;
-    }
-
-    private void Update()
-    {
-        UpdateClosestPositions();
-
-        for (var i = 0; i < images.Length; i++)
-            PlaceByIndex(i, positions[i]);
-    }
-
-    private void PlaceByIndex(int imageIndex, int index)
-    {
-        var pos = images[imageIndex].transform.position;
-        pos.x = size * index;
-        images[imageIndex].transform.position = pos;
-    }
-
-    private void UpdateClosestPositions()
-    {
-        positions[images.Length / 2] = GetClosestIndex();
-
-        for (var i = 1; i <= images.Length / 2; i++)
+        private void Update()
         {
-            positions[images.Length / 2 - i] = GetClosestIndex() - i;
+            UpdateClosestPositions();
 
-            if (images.Length / 2 + i <= images.Length - 1)
-                positions[images.Length / 2 + i] = GetClosestIndex() + i;
+            for (var i = 0; i < images.Length; i++)
+                PlaceByIndex(i, positions[i]);
         }
-    }
 
+        /// <summary>
+        /// Place the image by a position on the layer grid
+        /// </summary>
+        /// <param name="imageIndex">Number of the image</param>
+        /// <param name="index">Number of the position</param>
+        private void PlaceByIndex(int imageIndex, int index)
+        {
+            var pos = images[imageIndex].transform.position;
+            pos.x = size * index;
+            images[imageIndex].transform.position = pos;
+        }
 
-    private int GetClosestIndex()
-    {
-        var pos = Mathf.RoundToInt(character.position.x / size);
+        /// <summary>
+        /// Set the positions of the images each frame
+        /// </summary>
+        private void UpdateClosestPositions()
+        {
+            positions[images.Length / 2] = GetClosestIndex();
 
-        if (Vector2.Distance(new Vector2(pos * size, 0), character.position) >
-            Vector2.Distance(new Vector2((pos + 1) * size, 0), character.position))
-            pos++;
+            for (var i = 1; i <= images.Length / 2; i++)
+            {
+                positions[images.Length / 2 - i] = GetClosestIndex() - i;
 
-        return pos;
+                if (images.Length / 2 + i <= images.Length - 1)
+                    positions[images.Length / 2 + i] = GetClosestIndex() + i;
+            }
+        }
+
+        /// <summary>
+        /// Gets the index of the grid closest to the character
+        /// </summary>
+        /// <returns></returns>
+        private int GetClosestIndex()
+        {
+            var pos = Mathf.RoundToInt(character.position.x / size);
+
+            if (Vector2.Distance(new Vector2(pos * size, 0), character.position) >
+                Vector2.Distance(new Vector2((pos + 1) * size, 0), character.position))
+                pos++;
+
+            return pos;
+        }
     }
 }

6740a6d4ce6545b8bcde058c89366498627680c9
TopoArania <matufabbri@gmail.com>
2023-09-24 21:57:15 -0300

    Added attack delay

--- Desarrollo-3-2023/Assets/Code/Scripts/Attack/HitController.cs
+++ Desarrollo-3-2023/Assets/Code/Scripts/Attack/HitController.cs
@@ -11,28 +11,48 @@ namespace Code.Scripts.Attack
     public class HitController : MonoBehaviour
     {
         [SerializeField] private float damage = 10f;
+        [SerializeField] private float hitDelay = .2f;
         [SerializeField] private float hitDuration = 1f;
+        [SerializeField] private SpriteRenderer sprite;
         
         private readonly List<Damageable> hitObjects = new();
-        
+        private bool started;
+
         private void OnEnable()
         {
-            StartCoroutine(StopOnTime());
+            sprite.enabled = false;
+            
+            StartCoroutine(BeginAttackOnTime());
         }
 
         private void Update()
         {
+            if (!started) return;
+
             var trans = transform;
-            
+
             var hits = Physics2D.OverlapBoxAll(trans.position, trans.localScale, trans.rotation.z);
-            
+
             foreach (var hit in hits)
             {
                 if (!hit.TryGetComponent<Damageable>(out var damageable) || hitObjects.Contains(damageable)) continue;
-                
+
                 damageable.TakeDamage(damage);
                 hitObjects.Add(damageable);
             }
+        }
+
+        /// <summary>
+        /// Start attack after delay
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator BeginAttackOnTime()
+        {
+            yield return new WaitForSeconds(hitDelay);
+            
+            StartCoroutine(StopOnTime());
+            started = true;
+            sprite.enabled = true;
         }
 
         /// <summary>

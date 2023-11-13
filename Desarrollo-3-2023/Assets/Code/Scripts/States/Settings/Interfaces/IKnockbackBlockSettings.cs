9f195f31e346301a73e5b891e776ed76512defb4
ianco99 <71154684+ianco99@users.noreply.github.com>
2023-11-13 03:25:47 -0300

    Reorganized states SOs

---
+++ Desarrollo-3-2023/Assets/Code/Scripts/States/Settings/Interfaces/IKnockbackBlockSettings.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IKnockbackBlockSettings
+{
+    public float knockbackForce { get => knockbackForce; set => knockbackForce = value; }
+}

9640eee7b31106233a8367b4eb2074ca872958c0
ianco99 <71154684+ianco99@users.noreply.github.com>
2025-03-06 19:11:09 -0300

    Ray launcher raycasts towards target

--- Desarrollo-3-2023/Assets/Code/Scripts/Attack/RayLauncher.cs
+++ Desarrollo-3-2023/Assets/Code/Scripts/Attack/RayLauncher.cs
@@ -1,18 +1,36 @@
+using Code.Scripts.Abstracts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RayLauncher : MonoBehaviour
+public class RayLauncher : MonoBehaviour, IShooter
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private Transform spawnPos;
+
+    private Vector2 direction;
+    public Transform GetTransform()
     {
-        
+        return spawnPos.transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetAim(Vector2 aim)
     {
-        
+        direction = aim;
+    }
+
+    public void Shoot()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, Mathf.Infinity);
+
+        if (hit)
+        {
+            Debug.Log(hit.rigidbody.name);
+        }
+        else
+        {
+            Debug.Log("nope");
+        }
+        //direction;
     }
 }
